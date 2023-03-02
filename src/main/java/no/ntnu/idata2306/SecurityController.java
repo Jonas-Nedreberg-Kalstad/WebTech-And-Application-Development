a2e10925dc02ab0e83259ab3e65a3cdcacdcefa7
Edvin Astad <edvin.1996@hotmail.com>
2023-03-02 18:49:48 +0100

    Added Product and User class. I also added SecurityController class which might be refactored later depending on how password storage is handeled in the final application.

---
+++ src/main/java/no/ntnu/idata2306/SecurityController.java
@@ -0,0 +1,9 @@
+package no.ntnu.idata2306;
+
+public class SecurityController {
+
+    public static String generateSalt() {
+        // TODO implement an algorithm for generating salt.
+        return "0000";
+    }
+}
---
+++ src/main/java/no/ntnu/idata2306/User.java
@@ -0,0 +1,51 @@
+package no.ntnu.idata2306;
+
+public class User {
+
+    // TODO - implement database and create id field which represents user id.
+    private final String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private final String salt;
+
+    public User(String email, String firstName, String lastName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salt = SecurityController.generateSalt();
+        this.password = null;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    /**
+     * sets password to new password provided by user.
+     *
+     * <b>There will be no checks in this method as the server will be receiving the hashed password only.
+     * All checks to validate password must thus be done by website.</b>
+     *
+     * @param password hash which will correspond with user password + salt.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
