9a4f3f960f713201a40f4bc7fdaf8b7500d460df
DESKTOP-EONCIAU\Jonas <jonasned@stud.ntnu.no>
2023-04-05 07:08:09 +0200

    adding html and css to resources. Added dependencies in pom-file

--- src/main/java/no/ntnu/idata2306/controllers/PageController.java
+++ src/main/java/no/ntnu/idata2306/controllers/PageController.java
@@ -1,4 +1,27 @@
 package no.ntnu.idata2306.controllers;
 
+import no.ntnu.idata2306.services.ProductService;
+import no.ntnu.idata2306.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ */
 public class PageController {
+
+  ProductService productService;
+
+  UserService userService;
+
+  /**
+   *
+   * @param productService
+   * @param userService
+   */
+  public PageController(ProductService productService, UserService userService){
+    this.productService = productService;
+    this.userService = userService;
+
+  }
+
 }
