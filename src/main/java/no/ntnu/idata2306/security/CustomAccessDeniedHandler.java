424ec46cbd3d469ad85dfa9d7a14872a8b692b6a
DESKTOP-EONCIAU\Jonas <jonasned@stud.ntnu.no>
2023-05-29 02:08:25 +0200

    Wrote javadoc and fixed icon and title for access-denied and page-not-found .html

--- src/main/java/no/ntnu/idata2306/security/CustomAccessDeniedHandler.java
+++ src/main/java/no/ntnu/idata2306/security/CustomAccessDeniedHandler.java
@@ -8,7 +8,21 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 
 import java.io.IOException;
 
+/**
+ * CustomAccessDeniedHandler is an implementation of the AccessDeniedHandler interface.
+ * It handles access denied exceptions by redirecting the user to a designated access-denied page.
+ */
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+  /**
+   * Handles the access denied exception by setting the response status to 403 Forbidden
+   * and redirecting the user to the access-denied page.
+   *
+   * @param request The HttpServletRequest representing the current request.
+   * @param response The HttpServletResponse representing the response to be sent.
+   * @param accessDeniedException The AccessDeniedException that occurred.
+   * @throws IOException If an I/O error occurs during the redirect.
+   */
   @Override
   public void handle(HttpServletRequest request, HttpServletResponse response,
                      AccessDeniedException accessDeniedException) throws IOException {
