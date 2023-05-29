35faa45b42bbd7c488a9cbcd167d7c8d12fb0a69
TobiasGrav <tobiagra@stud.ntnu.no>
2023-05-29 07:46:08 +0200

    Created a .env file to declare environment variables inside the project, this file is added to the .gitignore.

--- src/test/java/no/ntnu/idata2306/ProfessionalWebsiteBackendApplicationTests.java
+++ src/test/java/no/ntnu/idata2306/ProfessionalWebsiteBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProfessionalWebsiteBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
